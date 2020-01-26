e913b529b66591fb444d9891d61a8f3a5868f48e
lesley <lesleybanadzem2017@gmail.com>
2020-01-25 16:55:11 -0800

    Formated code

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,27 +3,21 @@
   before granting access to the next middleware/route handler
 */
 const jwt = require('jsonwebtoken');
-const db = require('./../')
 const secret = process.env.JWT_SECRETE;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   
   if( authorization ) {
-    console.log(secret)
    jwt.verify(authorization, secret, (err, decoded)=>{
       if(err){
-        
         return res.status(403).json({
           message: "You are not authorized And should create an account to access dad jokes",
           err,
-          
       })
       }else {
-        console.log(decoded)
         next();
       }
 
     });
   }
-  // res.status(401).json({ you: 'shall not pass!' });
 };
